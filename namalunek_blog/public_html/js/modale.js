8e98bb0f8df28de1ecf7e944e7b78ef800d6fe42
xlpnsk <67523413+xlpnsk@users.noreply.github.com>
2020-07-20 23:38:45 +0200

    Add files via upload

---
+++ namalunek_blog/public_html/js/modale.js
@@ -0,0 +1,30 @@
+function showContent(i)
+{
+    var title=document.getElementById("ModalTitle");
+    var pic=document.getElementById("ModalPic");
+    var cont=document.getElementById("ModalContent");
+    switch(i)
+    {
+        case 1:
+        {
+            title.innerHTML="Zupagrafika&reg";
+            pic.src="https://images.adsttc.com/media/images/5d22/d809/284d/d163/a300/013b/newsletter/05._eastern-blocks-zupagrafika-berlin.jpg?1562564594";
+            cont.innerHTML="<h1>The post-war architecture of Eastern Bloc and beyond</h1><p>Zupagrafika are David Navarro and Martyna Sobecka, an independent publisher, author and graphic design studio, established in 2012 in Poznań, Poland, celebrating modernist architecture, design and photography in a unique and playful way. </p><p>Over the last decade, David and Martyna have created, illustrated and published award-winning books and kits exploring the post-war modernist and brutalist architecture of the former Eastern Bloc and beyond. Their publications have featured contributions by such architects and writers as: Norman Foster, John Grindrod, Barnabas Calder, Peter Chadwick, Christopher Beanland or Florian Heilmeyer. Zupagrafika are the authors and publishers of such titles as: Blok Wschodni (2014), Brutal London (2015), Blokoshka (2016), Paris Brut (2016), Blokowice (2016), The Constructivist (2017), Brutal East (2017), Modern East (2017), Hidden Cities (2018), as well as the books Brutal London (2016, Prestel), Brutal Britain (2018), Eastern Blocks (2019), Panelki (2019), Concrete Siberia (2020).</p><p>See the work of David and Martyna with your own eyes and see that beauty can be discovered even in the raw buildings of the previous century</p><p><a href='https://www.zupagrafika.com/'>Zupagrafika&reg official website</a></p>";
+            break;
+        }
+        case 2:
+        {
+            title.innerHTML="Pulp Fiction";
+            pic.src="https://i1.sndcdn.com/artworks-000174315981-m6g97v-t500x500.jpg";
+            cont.innerHTML="<p>Pulp Fiction is a 1994 American independent neo-noir crime film written and directed by Quentin Tarantino, who conceived it with Roger Avary.Starring John Travolta, Samuel L. Jackson, Bruce Willis, Tim Roth, Ving Rhames, and Uma Thurman, it tells several stories of criminal Los Angeles. The title refers to the pulp magazines and hardboiled crime novels popular during the mid-20th century, known for their graphic violence and punchy dialogue.</p><p>Pulp Fiction is widely regarded as Tarantino's masterpiece, with particular praise for its screenwriting.The self-reflexivity, unconventional structure, and extensive homage and pastiche have led critics to describe it as a touchstone of postmodern film. It is often considered a cultural watershed, influencing films and other media that adopted elements of its style. In 2008, Entertainment Weekly named it the best film since 1983 and it has appeared on many critics' lists of the greatest films ever made. In 2013, Pulp Fiction was selected for preservation in the United States National Film Registry by the Library of Congress as 'culturally, historically, or aesthetically significant'</p><p>[...]</p><p><a href=https://www.filmweb.pl/film/Pulp+Fiction-1994-1039'>Read more about 'Pulp Ficiotn'</a></p><p><a href='https://www.youtube.com/watch?v=s7EdQ4FqbhY' class='strip'>Watch the trailer!</a></p>";
+            break;
+        }
+        case 3:
+        {
+            title.innerHTML="The Museum of Games and Computers of the Past Era";
+            pic.src="https://media-cdn.tripadvisor.com/media/photo-s/19/84/60/b4/c64-w-muzeum-gry-i-komputery.jpg";
+            cont.innerHTML="<p></p><p>The Games and Computers Museum of the Past Era is a fan-made initiative created by four fascinates of old games and equipment with the help of our friends and family. We have a desire to share our passion and our collections with as many people as possible and the desire to infect them with our retro enthusiasm.</p><p>When creating our Museum, we were guided by the principle of combining the history of video entertainment and the history of computer science. So we have created a place where you can both view a number of unique exhibits, watch the showcases, and get to know  games – by playing them on nearly fifty stations (computers, consoles, arcade machines).</p><p>[...]</p><p>Learn more about The Games and Computers Museum of the Past Era on websites:</p><p><a href='https://gikme.pl/en'>The Museum of Games and Computers of the Past Era</a></p><p><a href='http://appleclub.pl/index.php/en/about-this-site/'>Apple Club</a></p>";        
+            break;
+        }
+    }
+}
